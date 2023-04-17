2223644d17a64f6954f2466c54ea90dda0931063
ssmpet <ssmpet@gmail.com>
2023-04-18 01:07:09 +0900

    웹디지인기능사연습

---
+++ 05.웹디자인기능사/01.JUST쇼핑몰/script/script.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    // 그룹 메뉴
+    $('nav>ul>li').mouseenter(function(){
+        $('ul.submenu').stop().slideDown(400);
+    });
+    $('nav>ul>li').mouseleave(function(){
+        $('ul.submenu').stop().slideUp(400);
+    });
+
+    // 위아래슬라이딩
+    // let x = 0;
+    setInterval (function(){
+        // if (x < 2)  x++;
+        // else x = 0;
+
+        // let sliderPosition = x * (-300) + "px";
+        // $('.sliderList').animate({top:sliderPosition}, 400);
+
+        $('.sliderList').animate({'margin-top':'-300'}, function(){
+            $('.sliderImg:first').appendTo('.sliderList')
+            $('.sliderList').css({'margin-top':'0'})
+        })
+    }, 3000);
+
+    // 탭메뉴
+    $('h2').on('click', function(){
+        $(this).addClass('on')
+        .siblings('h2').removeClass('on');
+    });
+
+});
