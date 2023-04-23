90a34a468f02884a5b02e9d7443103fafcbaed91
ssmpet <ssmpet@gmail.com>
2023-04-24 01:16:15 +0900

    웹디자인기능사

--- 05.웹디자인기능사/03.강원천문대/script/script.js
+++ 05.웹디자인기능사/03.강원천문대/script/script.js
@@ -1,9 +1,37 @@
 $(function(){
 
     // 그룹 메뉴
+    $('nav>ul>li').mouseover(function(){
+        $('.submenu').slideDown(100);
+    });
+    $('nav>ul>li').mouseleave(function(){
+        $('.submenu').slideUp(100)
+    });
 
     // slider 메뉴
+    $('.sliderImg:gt(0)').hide();
+    // var x = 0;
+    setInterval(function(){
+
+        // var next = (x + 1) %3;
+        $('.sliderImg:first').fadeOut(1500).next().fadeIn(1500);
+        $('.sliderImg:first').appendTo('.sliderList');
+        x = next;
+    }, 3000);
+
+    // 텝메뉴
+    $('h2').on('click', function(){
+        $(this).addClass('on')
+        .siblings('h2').removeClass('on');
+    });
 
     // 레이어 팝업
+    $('.layerPopup').on('click', function(){
+        $('#popup').fadeIn();
+    });
+
+    $('.close').on('click', function(){
+        $('#popup').fadeOut();
+    });
 
 });
