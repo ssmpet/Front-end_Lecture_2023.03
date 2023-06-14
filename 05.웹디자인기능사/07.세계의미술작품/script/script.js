9faa52db951204cfc464e09b931fa7950027c658
ssmpet <ssmpet@gmail.com>
2023-06-14 13:54:24 +0900

    웹디자인기능사

--- 05.웹디자인기능사/07.세계의미술작품/script/script.js
+++ 05.웹디자인기능사/07.세계의미술작품/script/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 
+    $('.mainmenu>li').mouseenter(function(){
+        $('.submenu').slideDown(100);
+    });
+    $('.mainmenu>li').mouseleave(function(){
+        $('.submenu').slideUp(100);
+    });
+
+    setInterval(function(){
+
+        $('.sliderList').animate({'margin-top':'-300'}, function(){
+            $('.sliderImg:first').appendTo('.sliderList')
+            $('.sliderList').css({'margin-top':'0'})
+        })
+    }, 3000);
+
+    $('.layerPopup').on('click', function(e){
+        e.preventDefault();
+        $('#popup').fadeIn();
+    });
+    $('.close').on('click', function(){
+        $('#popup').fadeOut();
+    });
 });
