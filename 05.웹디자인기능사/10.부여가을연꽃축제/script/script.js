f8947ce9c80f21a5bf9b588f5b63b3d54af810dc
ssmpet <ssmpet@gmail.com>
2023-07-28 17:25:52 +0900

    웹디자인기능사

--- 05.웹디자인기능사/10.부여가을연꽃축제/script/script.js
+++ 05.웹디자인기능사/10.부여가을연꽃축제/script/script.js
@@ -7,10 +7,20 @@ $(function(){
         function(){
             $(this).find('.submenu').stop().slideUp(500);
         });
-})
 
-$('.sliderImg:gt(0)').hide();
-setInterval(function(){
-    $('.sliderImg:first').fadeOut(1500).next().fadeIn(1500);
-    $('.sliderImg:first').appendTo('.sliderList')
-}, 3000);
+
+    $('.sliderImg:gt(0)').hide();
+    setInterval(function(){
+        $('.sliderImg:first').fadeOut(1500).next().fadeIn(1500);
+        $('.sliderImg:first').appendTo('.sliderList')
+    }, 3000);
+
+    $('.layerPopup').on('click', function(e){
+        e.preventDefault();
+        $('#popup').fadeIn();
+    });
+    $('.close').on('click', function(){
+        $('#popup').fadeOut();
+    });
+
+});
