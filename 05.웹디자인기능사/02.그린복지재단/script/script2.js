beb2402dc3161cd9aa3ab91e5e42c54603433955
ssmpet <ssmpet@gmail.com>
2023-08-01 17:40:21 +0900

    웹디자인기능사

--- 05.웹디자인기능사/02.그린복지재단/script/script2.js
+++ 05.웹디자인기능사/02.그린복지재단/script/script2.js
@@ -1,10 +1,27 @@
-$(function() {
-    $('.nav > ul > li').mouseover(function(){
-        $('.nav > ul > li > ul').stop().fadeIn(900);
-        // $('#header').addClass('on');
+$(function(){
+    $('.nav > ul > li').mouseover(function() {
+        $('.nav > ul > li > ul').stop().slideDown(400);
+        $('#header').addClass('on');
     });
-    $('.nav > ul > li').mouseout(function(){
-        $('.nav > ul > li > ul').stop().fadeOut(100);
-        // $('#header').removeClass('on');
+
+    $('.nav > ul > li').mouseout(function() {
+        $('.nav > ul > li > ul').stop().slideUp(100);
+        $('#header').removeClass('on');
     });
 });
+
+// window.onload = function() {
+//     let navList = document.querySelector('.nav > ul ');
+   
+//     navList.addEventListener('mouseover', function(){
+//         this.querySelectorAll('.submenu').forEach(element => {
+//             element.style.height = '165px';
+//         });
+//     });
+
+//     navList.addEventListener('mouseout', function(){
+//         this.querySelectorAll('.submenu').forEach(element => {
+//             element.style.height = '0px';
+//         });
+//     });
+// }
