de367e3e2d64a5f5fe6b2531bc8bbc5536bc977a
ssmpet <ssmpet@gmail.com>
2023-08-02 17:35:42 +0900

    웹디자인기능사

--- 05.웹디자인기능사/02.그린복지재단/script/script2.js
+++ 05.웹디자인기능사/02.그린복지재단/script/script2.js
@@ -1,27 +1,11 @@
 $(function(){
     $('.nav > ul > li').mouseover(function() {
-        $('.nav > ul > li > ul').stop().slideDown(400);
-        $('#header').addClass('on');
+        $('.nav > ul > li > ul, .background').stop().slideDown(400);
+        
     });
 
     $('.nav > ul > li').mouseout(function() {
-        $('.nav > ul > li > ul').stop().slideUp(100);
-        $('#header').removeClass('on');
+        $('.nav > ul > li > ul, .background').stop().slideUp(100);
+        
     });
 });
-
-// window.onload = function() {
-//     let navList = document.querySelector('.nav > ul ');
-   
-//     navList.addEventListener('mouseover', function(){
-//         this.querySelectorAll('.submenu').forEach(element => {
-//             element.style.height = '165px';
-//         });
-//     });
-
-//     navList.addEventListener('mouseout', function(){
-//         this.querySelectorAll('.submenu').forEach(element => {
-//             element.style.height = '0px';
-//         });
-//     });
-// }
--- 05.웹디자인기능사/04.유진건설/script/script2.js
+++ 05.웹디자인기능사/04.유진건설/script/script2.js
@@ -1,12 +1,12 @@
 $(function() {
 
     $('.nav > ul > li').mouseover(function() {
-        $('.nav > ul > li > ul').stop().slideDown(500);
-        $('#header').addClass('on');
+        $('.nav > ul > li > ul, .background').stop().slideDown(500);
+
     });
 
     $('.nav > ul > li').mouseout(function() {
-        $('.nav > ul > li > ul').stop().slideUp(200);
-        $('#header').removeClass('on');
+        $('.nav > ul > li > ul, .background').stop().slideUp(200);
+
     });
 });
