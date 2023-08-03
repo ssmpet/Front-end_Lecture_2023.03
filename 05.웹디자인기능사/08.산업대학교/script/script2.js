5aaa60e68852ec43f7611413d863acc78a1da65a
ssmpet <ssmpet@gmail.com>
2023-08-03 14:33:44 +0900

    웹디자인기능사

--- 05.웹디자인기능사/08.산업대학교/script/script2.js
+++ 05.웹디자인기능사/08.산업대학교/script/script2.js
@@ -1,9 +1,9 @@
 $(function(){
     $('.nav > ul > li').mouseover(function(){
-        $('.nav > ul > li > ul').stop().slideDown();
+        $('.nav > ul > li > ul, .background').stop().slideDown();
     });
 
     $('.nav > ul > li').mouseout(function(){
-        $('.nav > ul > li > ul').stop().slideUp();
+        $('.nav > ul > li > ul, .background').stop().slideUp();
     });
 });
