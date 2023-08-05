1729a2e455000dc1ae211e60e5b17b58d712741c
ssmpet <ssmpet@gmail.com>
2023-08-05 17:09:07 +0900

    웹디자인기능사

---
+++ 05.웹디자인기능사/13.조이컨트리클럽/script/script.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    $('.nav > ul > li').hover(
+        function(){
+            $(this).find('.submenu').stop().slideDown();
+        }, 
+        function() {
+            $(this).find('.submenu').stop().slideUp();
+        }
+    );
+
+    setInterval(function(){
+
+        $('.sliderWrap').animate({marginTop : '-400'}, function(){
+            $('.sliderImg:first').appendTo('.sliderWrap');
+            $('.sliderWrap').css({marginTop : '0'});
+        })
+    }, 3000);
+
+    let tabBtn = $('.tab-btn > ul > li');
+    let tabCont = $('.tab-cont > div');
+
+    tabCont.hide().eq(0).show();
+    tabBtn.click(function(){
+        const index = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+});
