14464806714ad463438b0327989fb91fa17fe6c2
ssmpet <ssmpet@gmail.com>
2023-08-10 12:34:20 +0900

    웹디자인기능사

--- 05.웹디자인기능사/18.역사박물관/script/script.js
+++ 05.웹디자인기능사/18.역사박물관/script/script.js
@@ -1,17 +1,38 @@
 $(function(){
 
-    let currentIndex = 0;
+    $('.nav > ul > li').hover(
+        function(){
+            $(this).find('.submenu').stop().slideDown();
+        }, 
+        function(){
+            $(this).find('.submenu').stop().slideUp();
 
-    $('.sliderWrap').append($('.slider').first().clone(true));
+        }
+    );
 
+    $('.sliderWrap').append( $('.sliderImg').first().clone(true));
+    let  currentIndex = 0;
     setInterval(function(){
         currentIndex++;
-        $('.sliderWrap').animate({marginTop: -currentIndex*100 + "vh"}, 600);
-        if (currentIndex == 3) {
+
+        $('.sliderWrap').animate({marginTop: -(currentIndex * 700) + "px"}, 600);
+        
+        if(currentIndex==3) {
             setTimeout(function(){
-                $('.sliderWrap').animate({marginTop: 0}, 0);
-                currentIndex = 0;
-            }, 700);
+                $('.sliderWrap').animate({marginTop:0}, 0);
+                currentIndex=0;
+            });
         }
     }, 3000);
+
+
+    $('.popupLayer').click(function(){
+        $('#popup').show();
+    });
+
+    
+    $('.close').click(function(){
+        $('#popup').hide();
+    });
+
 });
