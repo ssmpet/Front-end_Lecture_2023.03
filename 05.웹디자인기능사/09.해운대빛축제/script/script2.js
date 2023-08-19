37f638ee91c189fb3fa63ab098dac6c46478adae
ssmpet <ssmpet@gmail.com>
2023-08-19 17:49:51 +0900

    웹디자인기능사

--- 05.웹디자인기능사/09.해운대빛축제/script/script2.js
+++ 05.웹디자인기능사/09.해운대빛축제/script/script2.js
@@ -7,4 +7,19 @@ $(function(){
             $(this).find('.submenu').stop().slideUp(400);
          }
     );
+
+    let currentIndex = 0;
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % 3;
+        $('.sliderImg').eq(currentIndex).fadeOut(1500);
+        $('.sliderImg').eq(nextIndex).fadeIn(1500);
+        currentIndex = nextIndex;
+    }, 3000);
+
+    $('.con_btn > div').click(function(){
+        let n = $(this).index();
+        $(this).css('background-color', "#db6666").siblings().css('background-color', "#dfdfdf");
+        $('.con_con > div').eq(n).show().siblings().hide(); 
+
+    });
 });
