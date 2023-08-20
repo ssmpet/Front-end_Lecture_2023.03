1a55b4b5e5f6891a557cd7a9817b3fe132deb214
ssmpet <ssmpet@gmail.com>
2023-08-20 21:44:50 +0900

    웹디자인기능사

--- 05.웹디자인기능사/06.대한투어/script/script2.js
+++ 05.웹디자인기능사/06.대한투어/script/script2.js
@@ -1,9 +1,31 @@
-$(document).ready(function() {
+$(function(){
 
-    $('.nav > ul > li').mouseover(function(){
-        $('.submenu, .background').stop().slideDown(200);
+    $('.nav > ul > li').hover( 
+        function(){
+            $('.nav > ul > li > ul, .background').stop().slideDown(400);
+        }, 
+        function(){
+            $('.nav > ul > li > ul, .background').stop().slideUp(400);
+        }
+    );
+
+    $('.sliderWrap').append($('.sliderImg').first().clone(true));
+    let currentIndex = 0;
+    setInterval(function(){
+        currentIndex++;
+        $('.sliderWrap').animate({marginTop: -(currentIndex * 300) + "px"}, 600);
+
+        if ( currentIndex == 3 ){
+            $('.sliderWrap').animate({marginTop: 0}, 0);
+            currentIndex = 0;
+        }
+    }, 3000);
+
+    $('.layerPopup').click(function(){
+        $('#popup').show();
     });
-    $('.nav > ul > li').mouseout(function(){
-        $('.submenu, .background').stop().slideUp(200);
+
+    $('.close').click(function(){
+        $('#popup').hide();
     });
 });
